68a567b187fcd17ddaeea9aa7788954561f998fa
Rijad99 <rijad99.sm@gmail.com>
2023-12-09 14:30:59 +0100

    Adapt layout CSS, add localizations, add 2 more task statuses

--- src/pages/dashboard/components/sidebar/components/projects/Projects.tsx
+++ src/pages/dashboard/components/sidebar/components/projects/Projects.tsx
@@ -4,6 +4,10 @@ import { useContext } from "react"
 // Components
 import Accordion from "../../../../../../common/components/accordion/Accordion"
 import ProjectItems from "./components/project-items/ProjectItems"
+import Button from "../../../../../../common/components/button/Button.tsx"
+
+// CSS
+import projectsCSS from "./Projects.module.scss"
 
 // Data
 import { allProjectsData } from "./utils/projects-mock-data"
@@ -14,11 +18,37 @@ import { LocalizationContext } from "../../../../../../common/context/Localizati
 // Types
 import { ProjectsProps } from "./Projects.types"
 
+// ENUMS
+import {
+	ButtonSize,
+	ButtonType,
+} from "../../../../../../common/components/button/Button.types.ts"
+import Svg from "../../../../../../common/components/svg/Svg.tsx"
+import { plusIcon } from "../../../../../../common/icons/icons.ts"
+import { SvgColors } from "../../../../../../common/components/svg/Svg.types.ts"
+
 function Projects({ onProjectChange }: ProjectsProps) {
 	const { localization } = useContext(LocalizationContext)
 
+	const handleProjectCreation = () => {}
+
 	return (
 		<Accordion title={localization.projects}>
+			<Button
+				type={ButtonType.PRIMARY}
+				size={ButtonSize.LARGE}
+				onClick={handleProjectCreation}
+				additionalClasses={projectsCSS.createProjectButton}
+			>
+				{localization.createNewProject}
+				<Svg
+					path={plusIcon}
+					width="26"
+					height="13"
+					viewBox="0 0 9 14"
+					color={SvgColors.NEUTRAL}
+				/>
+			</Button>
 			<ProjectItems
 				projects={allProjectsData}
 				onProjectChange={onProjectChange}
